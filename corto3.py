998bf3a38c5e0f5ed2335c0f8e2bd9b7cbea38d9
Angelo <27garciaangelo@gmail.com>
2025-07-29 19:50:53 -0600

    Ejercicio 3 terminado

--- corto3.py
+++ corto3.py
@@ -3,5 +3,16 @@ productos  = []
 for i in range(3): 
     print( f"\n Producto {i+1}: " )
     nombre = input("Nombre del producto: ")
-    precio = int(input("Precio del producto Q: "))
+    precio = int(input("Precio del producto en Q: "))
     stock = int(input("Cantidad en stock: "))
+
+    producto = {
+        "nombre" : nombre,
+        "precio" : precio,
+        "stock": stock
+    }
+    productos.append(producto)
+
+    print("\n Productos")
+    for j in productos:
+        print( f"El producto {j['nombre']} cuesta {j['precio']} y quedan {j['stock']} unidades disponibles" )
