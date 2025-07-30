a73c1a87525de4fa63e6bff7bd6e90fcc11b23dd
Angelo <27garciaangelo@gmail.com>
2025-07-29 19:44:26 -0600

    Primer commit corto3

---
+++ corto3.py
@@ -0,0 +1,7 @@
+productos  = []
+
+for i in range(3): 
+    print( f"\n Producto {i+1}: " )
+    nombre = input("Nombre del producto: ")
+    precio = int(input("Precio del producto Q: "))
+    stock = int(input("Cantidad en stock: "))
